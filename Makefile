461b57dfd79be2a158d6681a0e0210dcaa15f144
Florian Buestgens <fb@fx-ttr.de>
2022-08-04 14:45:19 +0200

    Moving to lib

--- Makefile
+++ Makefile
@@ -8,11 +8,11 @@ all: compile
 
 compile:
 	@echo "Compiling kernel"
-	@-cargo build
+	@-cargo build 
 
 link:
 	@echo "Linking kernel"
-	@ld -T src/link.ld ${BUILD_DIR}/s2.o target/x86_64-unknown-lumos/debug/liblumos.a -o ${BUILD_DIR}/lumos
+	@ld -T src/link.ld ${BUILD_DIR}/s2.o target/x86_64-unknown-lumos/${PROFILE}/liblumos.rlib -o ${BUILD_DIR}/lumos
 
 install-bootloader: ${IMG_FILE}
 	@echo "Installing Syndicate"
--- build.rs
+++ build.rs
@@ -20,7 +20,7 @@ use std::env;
 use std::process::Command;
 
 fn main() {
-    let out_dir = env::var_os("OUT_DIR").expect("Something is really wrong. I could not find the OUT_DIR variable.");
+    //let out_dir = env::var_os("OUT_DIR").expect("Something is really wrong. I could not find the OUT_DIR variable.");
     
     // Edit me!
     let asm_files: [&str; 1] = ["src/arch/amd64/boot/s2.nasm"];
@@ -30,16 +30,17 @@ fn main() {
     // Do not edit me.
     let build_dir_arg = env::current_dir().unwrap().join(build_dir);
     let arch_arg: String = "-f".to_owned() + tarch;
-
+    
     for file in asm_files {
-	let object_file = file.split("/").last().unwrap().split(".").nth(0).unwrap().to_owned();
+       	    let object_file = file.split("/").last().unwrap().split(".").nth(0).unwrap().to_owned();
 	let object_file_path = build_dir_arg.join(object_file + ".o");
 	
-	Command::new("nasm")
-	    .arg(&arch_arg[..])
-	    .arg(file)
-	    .args(&["-o", object_file_path.to_str().unwrap()])
-	    .status().unwrap();
+	    Command::new("nasm")
+	        .arg(&arch_arg[..])
+	        .arg(file)
+	        .args(&["-o", object_file_path.to_str().unwrap()])
+	        .status().unwrap();
+
     }
 
     println!("cargo:rerun-if-changed=build.rs");
