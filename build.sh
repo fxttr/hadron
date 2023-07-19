a411fec21ad082a98937ee6fde241ac7441f790b
Florian Marrero Liestmann <f.m.liestmann@fx-ttr.de>
2023-07-19 06:21:35 +0200

    Fixing toolchain

--- build.sh
+++ build.sh
@@ -2,25 +2,35 @@
 
 # var
 OS="FreeBSD"
+BUILD_DIR=./build
+PROFILE=debug
+CLANGFLAGS="-Wall -O2 -ffreestanding -nostdinc -nostdlib -mcpu=cortex-a72+nosimd"
 
 if [ ! $(command -v zypper) ]; then
   OS="OpenSUSE"
-elif [ ! $(command -v apt) ]; then
-  OS="Debian"
 fi
 
 # fn
-function build_kernel() {
+function compile() {
+    clang --target=aarch64-elf $CLANGFLAGS -c ./src/arch/aarch64/boot/boot.S -o $BUILD_DIR/boot.o
     cargo build
+}
+
+function link() {
+    ld -m aarch64elf -nostdlib -T src/link.ld $BUILD_DIR/boot.o target/aarch64-unknown-lumos/$PROFILE/liblumos_rtl4.a -o $BUILD_DIR/lumos
+}
+
+function build_kernel() {
+    compile
+    link
 }
 
 function build_image() {
     echo "IMG"
 }
 
-# main
-build_kernel
-
-if [ $1 == "IMAGE" ]; then
-  build_image
+if [ ! -d "$BUILD_DIR" ]; then
+    mkdir $BUILD_DIR
 fi
+
+"$@"
