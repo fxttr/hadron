80c2bb83490cc67a958c2e9f7b70da4045410f19
Florian Büstgens <fb@fx-ttr.de>
2023-07-23 00:13:26 +0200

    [SECURITY] Implementing segmentation and plevel

--- domains/gdt/src/lib.rs
+++ domains/gdt/src/lib.rs
@@ -15,16 +15,73 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+use security::segmentation::{Descriptor, SegmentSelector};
+
 pub struct Gdt {
-    table: [u64; 10],
-    next: usize,
+    table: [u64; 8],
+    len: usize,
 }
 
 impl Gdt {
     pub const fn new() -> Self {
         Self {
-            table: [0; 10],
-            next: 1, // entry 0 must be NULL
+            table: [0; 8],
+            len: 1, // entry 0 must be NULL
         }
+    }
+
+    #[inline]
+    pub fn as_raw_slice(&self) -> &[u64] {
+        &self.table[..self.len]
+    }
+
+    #[inline]
+    pub const unsafe fn u_from_raw_slice(slice: &[u64]) -> Gdt {
+        let len: usize = slice.len();
+        let mut table: [u64; 8] = [0; 8];
+        let mut i: usize = 0;
+
+        assert!(len <= 8, "A GDT cannot be longer than 8 elements.");
+
+        while len > i {
+            table[i] = slice[i];
+            i += 1;
+        }
+
+        Gdt { table, len }
+    }
+
+    #[inline]
+    fn push(&mut self, value: u64) -> usize {
+        let i = self.len;
+        
+        self.table[i] = value;
+        self.len += 1;
+
+        i
+    }
+
+    #[inline]
+    pub fn add(&mut self, entry: Descriptor) -> SegmentSelector {
+        let i = match entry {
+            Descriptor::SystemSegment(low, high) => {
+                if self.len > self.table.len().saturating_sub(2) {
+                    panic!("Not enough space in GDT for holding a SystemSegment.")
+                }
+
+                let i = self.push(low);
+                self.push(high);
+                i
+            }
+            Descriptor::UserSegment(value) => {
+                if self.len > self.table.len().saturating_sub(1) {
+                    panic!("Not enough space in GDT for holding a UserSegment.")
+                }
+
+                self.push(value)
+            }
+        };
+
+        SegmentSelector(i as u16)
     }
 }
