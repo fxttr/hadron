c037070762afcda4241844436ddc536056755736
Florian Marrero Liestmann <f.m.liestmann@fx-ttr.de>
2023-07-26 20:33:15 +0200

    Implementing segmentation and plevels

---
+++ domains/security/src/core/privileges.rs
@@ -0,0 +1,39 @@
+/*
+ * This file is part of the Zen distribution (https://github.com/fxttr/zen).
+ * Copyright (c) 2023 Florian Marrero Liestmann.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/// Represents a protection ring level.
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+#[repr(u8)]
+pub enum PLevel {
+    Ring0 = 0,
+    Ring1 = 1,
+    Ring2 = 2,
+    Ring3 = 3,
+}
+
+impl PLevel {
+    #[inline]
+    pub const fn from_u16(value: u16) -> PLevel {
+        match value {
+            0 => PLevel::Ring0,
+            1 => PLevel::Ring1,
+            2 => PLevel::Ring2,
+            3 => PLevel::Ring3,
+            _ => panic!("invalid PLevel"),
+        }
+    }
+}
---
+++ domains/security/src/core/segmentation.rs
@@ -0,0 +1,64 @@
+/*
+ * This file is part of the Zen distribution (https://github.com/fxttr/zen).
+ * Copyright (c) 2023 Florian Marrero Liestmann.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+use super::privileges::PLevel;
+use bit_field::BitField;
+
+pub enum Descriptor {
+    UserSegment(u64),
+    SystemSegment(u64, u64),
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[repr(transparent)]
+pub struct SegmentSelector(pub u16);
+
+pub struct CodeSegment;
+
+pub struct StackSegment;
+
+pub struct DataSegment;
+
+pub struct FSegment;
+
+pub struct GSegment;
+
+impl SegmentSelector {
+    pub const NULL: Self = Self::new(0, PLevel::Ring0);
+
+    #[inline]
+    pub const fn new(index: u16, rpl: PLevel) -> Self {
+        Self(index << 3 | (rpl as u16))
+    }
+
+    #[inline]
+    pub fn index(self) -> u16 {
+        self.0 >> 3
+    }
+
+    // Requested Privilege Level ()
+    #[inline]
+    pub fn rpl(self) -> PLevel {
+        PLevel::from_u16(self.0.get_bits(0..2))
+    }
+
+    /// Set the privilege level for this Segment selector.
+    #[inline]
+    pub fn set_rpl(&mut self, rpl: PLevel) {
+        self.0.set_bits(0..2, rpl as u16);
+    }
+}
